514019fe542fcd5a129021b1f4217afe641f93ca
droopyantidote <144815313+droopyantidote@users.noreply.github.com>
2024-02-13 11:13:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-//your JS code here. If required.
+function secondHighest(arr) {
+    if (arr.length <= 1 || new Set(arr).size === 1) {
+        return -Infinity;
+    }
+
+    let firstMax = -Infinity;
+    let secondMax = -Infinity;
+
+    for (let num of arr) {
+        if (num > firstMax) {
+            secondMax = firstMax;
+            firstMax = num;
+        } else if (num > secondMax && num < firstMax) {
+            secondMax = num;
+        }
+    }
+
+    return secondMax;
+}
+
+// Test cases
+console.log(secondHighest([5, 1, 2, 3, 4]));   // Output: 4
+console.log(secondHighest([-1, -2, -3, -4, -5]));   // Output: -2
+console.log(secondHighest([]));   // Output: -Infinity
+console.log(secondHighest([1]));   // Output: -Infinity
+console.log(secondHighest([1, 1, 1, 1, 1]));   // Output: -Infinity
